5718948e2992904af5a86b9ca816e737cb789147
Sergey Kislyakov <s.kislyakov84@gmail.com>
2025-01-16 17:52:03 +0300

    Create database library_db

---
+++ app/api/database.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+# Настройки базы данных
+DATABASE_URL = "postgresql+psycopg2://coder:password@localhost:5432/library_db"
+
+# Создание подключения
+engine = create_engine(DATABASE_URL)
+
+# Создание сессии для взаимодействия с базой данных
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Базовый класс для декларативных моделей
+Base = declarative_base()
+
+# Зависимость для получения сессии
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ app/models/user.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from app.api.database import Base
+
+class User(Base):
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    role = Column(String, default="reader") # роль пользователя (администратор или читатель)
